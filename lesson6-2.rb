0c19f3442d1ee153f5b11f9ed10031aca21152d4
EC2 Default User <112754440+koubasii@users.noreply.github.com>
2022-09-14 19:56:37 +0000

    finish

---
+++ lesson6-2.rb
@@ -0,0 +1,25 @@
+puts "計算をはじめます"
+puts "何回繰り返しますか？"
+
+i = gets.to_i
+
+x = 1
+while x <= i do
+puts "#{x}回目の計算"
+puts "2つの値を入力してください"
+
+a = gets.to_i
+b = gets.to_i
+
+puts "a=#{a}"
+puts "b=#{b}"
+
+puts "計算結果を出力します"
+puts "a+b=#{a + b}"
+puts "a-b=#{a - b}"
+puts "a*b=#{a * b}"
+puts "a/b=#{a / b}"
+
+x +=1
+
+end
